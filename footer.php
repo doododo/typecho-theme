5dc0dda68ff52381af91a9f1b4878cb0e43379f2
宋其健 <songqijian@chehejia.com>
2017-09-06 18:09:25 +0800

    去掉统计

--- footer.php
+++ footer.php
@@ -19,6 +19,5 @@
 <script>var scroll = new SmoothScroll('a[href*="#"]', {offset: 100});</script>
 
 <?php $this->footer(); ?>
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1255150612'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1255150612' type='text/javascript'%3E%3C/script%3E"));</script>
 </body>
 </html>
