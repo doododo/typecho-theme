6828bcf45a2d7c55abb30c5003e6aa1f8a8d71f3
bigface <saybye720@gmail.com>
2018-06-08 18:38:21 +0800

    二维码支付

--- page.php
+++ page.php
@@ -1,18 +1,43 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
- 
+<?php
+$ua = $_SERVER['HTTP_USER_AGENT'];
+if (strpos($ua, 'MicroMessenger')) {
+    $type = 'wepay';
+    $name = '微信支付';
+    //微信支付链接
+    $url = 'wxp://f2f0kTXjXbW-QPIlNdRDLlz87ZD29FBDbaJo';
+    $icon_img = '<img src="http://ww2.sinaimg.cn/large/005zWjpngy1fojrwgr20oj303k03kglg.jpg" width="48px" height="48px" alt="'.$name.'">';
+}
+elseif (strpos($ua, 'AlipayClient')) {
+    //支付宝链接
+    $url = 'https://qr.alipay.com/tsx09551sst2svw7kicy185';
+    header('location: ' . $url);
+}
+elseif (strpos($ua, 'QQ/')) {
+    $type = 'qq';
+    $name = 'QQ钱包支付';
+    //QQ钱包支付链接
+    $url = 'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&a=1&u=82220797&ac=2387E199035108BB71E6FD631077C2991ACC07648E4EBE0BC9ABEAFDF1549364&n=%E5%A4%A7%E8%84%B8%20%20%E1%83%A6%EF%BE%9F&f=wallet';
+    $icon_img = '<img src="http://ww2.sinaimg.cn/large/005zWjpngy1fojrvmp427j303k03kjrb.jpg" width="48px" height="48px" alt="'.$name.'">';
+}
+else {
+    $type = 'other';
+    $name = '打赏作者';
+    $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    $icon_img = '<img src="http://ww2.sinaimg.cn/large/005zWjpngy1fojs089x6tj303k03kjr6.jpg" width="48px" height="48px" alt="'.$name.'">';
+}
+$qr_img = '<img src="http://qr.liantu.com/api.php?text='.urlencode($url).'">';
+?>
 <main class="container">
     <div class="wrap min">
         <section class="page-title">
-            <h2><?php $this->title() ?></h2>
-            <?php if($this->authorId == $this->user->uid): ?> <a class="edit-link" href="<?php $this->options->adminUrl(); ?>write-page.php?cid=<?php echo $this->cid;?>" target="_blank">编辑</a><?php endif; ?>
+            <?=$icon_img?>
+            <h2><?php echo $name ?></h2>
         </section>
         <article class="page-content">
-            <?php $this->content(); ?>
+            <?=$type=='other'?$qr_img.'<h4>请使用支付宝、微信、QQ客户端扫码付款</h4>':$qr_img.'<h4>扫描或长按识别二维码，向TA付款</h4>'?>
         </article>
-        
-        <?php $this->need('comments.php'); ?>
-        
     </div>
 </main>
 
